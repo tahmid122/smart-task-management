11c15f77eec8e658493c1678cdf92c77af86dd8f
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-11-18 22:56:06 +0600

    projects and task total done

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecentReAssignments from "../RecentReAssignments/RecentReAssignments";
 import TeamSummary from "../TeamSummary/TeamSummary";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const Dashboard = () => {
+  const { user } = useAuth();
+  const { axiosSecure } = useAxiosSecure();
+  const [total, setTotal] = useState({ projectLength: 0, taskLength: 0 });
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const { data } = await axiosSecure.get(`/total/${user.email}`);
+        if (data?.success) {
+          setTotal(data?.data);
+        } else {
+          console.log(data?.message);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    })();
+  }, [user, axiosSecure]);
+  console.log(total);
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
@@ -13,19 +34,18 @@ const Dashboard = () => {
       </div>
       <div className="flex items-center gap-4 justify-center">
         <div className="p-5 rounded-md border border-slate-100 shadow-sm flex items-center justify-center gap-2 flex-col w-[150px]">
-          <h2 className="text-3xl font-bold">10</h2>
+          <h2 className="text-3xl font-bold">{total?.projectLength}</h2>
           <p className="font-semibold text-green-700">Total Projects</p>
         </div>
         <div className="p-5 rounded-md border border-slate-100 shadow-sm flex items-center justify-center gap-2 flex-col w-[150px]">
-          <h2 className="text-3xl font-bold">10</h2>
+          <h2 className="text-3xl font-bold">{total?.taskLength}</h2>
           <p className="font-semibold text-orange-400">Total Tasks</p>
         </div>
       </div>
       <div>
         <RecentReAssignments />
       </div>
-      <div>
-        <h2 className="text-2xl font-semibold mb-5 mt-10">Team Summary</h2>
+      <div className="mb-5 mt-10">
         <TeamSummary />
       </div>
     </div>
--- src/Pages/TeamSummary/TeamSummary.jsx
+++ src/Pages/TeamSummary/TeamSummary.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
 
 const TeamSummary = () => {
+  const { axiosSecure } = useAxiosSecure();
+  const [teamSummary, setTeamSummary] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    (async function () {
+      setIsLoading(true);
+      try {
+        const { data } = await axiosSecure("/team-summary");
+        if (data?.success) {
+          setTeamSummary(data?.data);
+        } else {
+          console.log(data?.message);
+        }
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [axiosSecure]);
+  if (isLoading) return <LoadingSpinner />;
   return (
     <div>
       {" "}
@@ -8,29 +31,41 @@ const TeamSummary = () => {
       <div className="space-y-10">
         {/* single team */}
         <div>
-          <h2 className="text-xl font-bold">Team Alpha</h2>
+          <h2 className="text-2xl font-semibold">Team Summary</h2>
           <div className="overflow-x-auto mt-5 rounded-box border border-base-content/5 bg-base-100">
             <table className="table">
               {/* head */}
               <thead>
                 <tr>
+                  <th>Team</th>
                   <th>Name</th>
                   <th className="text-center">Current Task</th>
                   <th className="text-center">Capacity</th>
                 </tr>
               </thead>
               <tbody>
-                {/* row 1 */}
-                <tr>
-                  <td>Cy Ganderton</td>
-                  <td className="text-center">3</td>
-                  <td className="text-center">5</td>
-                </tr>
-                <tr className="bg-red-500 font-bold text-white">
-                  <td>Cy Ganderton</td>
-                  <td className="text-center">7</td>
-                  <td className="text-center">5</td>
-                </tr>
+                {teamSummary && teamSummary.length > 0 ? (
+                  teamSummary.map((summary, index) => (
+                    <tr
+                      key={index}
+                      className={`${
+                        summary?.currentTask > summary?.capacity &&
+                        "bg-red-500 text-white"
+                      }`}
+                    >
+                      <td>{summary?.teamName}</td>
+                      <td>{summary?.name}</td>
+                      <td className="text-center">{summary?.currentTask}</td>
+                      <td className="text-center">{summary?.capacity}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={12} className="text-center">
+                      No data found
+                    </td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
