63bc57eb0e3e13c144024a93fd43805810af2533
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-11-19 09:21:05 +0600

    added vercel.json

--- src/Pages/RecentReAssignments/RecentReAssignments.jsx
+++ src/Pages/RecentReAssignments/RecentReAssignments.jsx
@@ -6,17 +6,16 @@ const RecentReAssignments = () => {
   return (
     <div>
       <h2 className="text-2xl font-semibold">
-        Recent Re-Assignments Tasks (5)
+        Recent Re-Assignments Tasks (0)
       </h2>
       {/* all tasks */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-10">
-        {/* single task */}
+      {/* <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-10">
         <RecentCard />
         <RecentCard />
         <RecentCard />
         <RecentCard />
         <RecentCard />
-      </div>
+      </div> */}
     </div>
   );
 };
